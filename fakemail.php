f98b8642bd82d4b68e3c2a249705979603b4768e
Callum Jones <cjdeltacallumjones@gmail.com>
2014-02-10 12:02:30 +0000

    Update fakemail.php

--- fakemail.php
+++ fakemail.php
@@ -1,6 +1,19 @@
+<?php
+ $email = $_POST['email'];
+$content = nl2br($_POST['content']);
+$name = $_POST['name'];
+$sender = $_POST['sender'];
+$subject = $_POST['subject'];
+$headers = "From: $name "."<".$sender.">\r\n";
+//add boundary string and mime type specification
+//$headers .= 'MIME-Version: 1.0' . "\r\n";
+ //$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+//send the email
+mail($email, $subject, $content, $headers );
+ ?>
 <html>
 <head>
-<meta http-equiv="refresh" content="3; url=http://www.devgutter.com/mail/index.html" />
+<meta http-equiv="refresh" content="3; url=/index.html" />
 </head>
 <center>
 <h1>Message sent! Redirecting you back to the form now!</h1>
