edf61a8b5b2dc0c0761894e6279daa1a6bf6d26b
Callum Jones <cjdeltacallumjones@gmail.com>
2014-02-10 10:01:10 +0000

    Added the file, this is what sends it from the server!
    The file which sends the information from the server!

---
+++ fakemail.php
@@ -0,0 +1,6 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="3; url=http://www.devgutter.com/mail/index.html" />
+</head>
+<center>
+<h1>Message sent! Redirecting you back to the form now!</h1>
